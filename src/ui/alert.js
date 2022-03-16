84de2713284aee2749a2df1bc48101147ed894e1
Anton Balter <antbalter@gmail.com>
2022-03-16 21:46:30 +0200

    HW#28

--- src/ui/alert.js
+++ src/ui/alert.js
@@ -8,7 +8,10 @@ export default class Alert {
     showAlert() {
         this.#alertElem.innerHTML = `
         <div class="alert alert-danger alert-dismissible fade show mt-5" role="alert">
-            <strong>Error!</strong> OOPS!!! d[o_0]b <br> The server <a href=http://localhost:3500/courses>http://localhost:3500/courses</a> is unavailable! Please, repeat request later on.
+            <strong>Error!</strong> OOPS!!! d[o_0]b <br> Unfortunately, the server <a href=http://localhost:3500/courses>http://localhost:3500/courses</a> could not be reached
+            at this very moment. <br> Please, kind Sir/Madame, could you be so kind to repeat your most desired request at a later time? <br><br>
+            Your's sincerely,<br>
+            Beep Boop.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>`;
     }
